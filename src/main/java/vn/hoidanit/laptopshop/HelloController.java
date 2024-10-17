1d29600dc0afa07cda43d20d7488526614b3d63f
taokobik1234 <thienvovinpro123@gmail.com>
2024-10-17 22:50:56 +0700

    test1

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String index() {
-        return "Hello World from !";
+        return "Hello World from VN !";
+    }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Hello World from user !";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Hello World from admin !";
     }
 }
